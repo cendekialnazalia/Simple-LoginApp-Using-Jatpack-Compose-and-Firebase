82f7161503de4291e1d8b4da379245b1d1d3693e
cendekialnazalia <cendekialnazalia@gmail.com>
2023-10-10 01:05:01 +0700

    first commit

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.loginapp.login.LoginViewModel
+import com.example.loginapp.ui.theme.LoginAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val loginViewModel = viewModel(modelClass = LoginViewModel::class.java)
+            LoginAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Navigation(loginViewModel = loginViewModel)
+                }
+            }
+        }
+    }
+}
